a79c844235e38d2b3d2ced1bfd423fa133faaf47
itsInziani <inzianihope@gmail.com>
2023-03-17 05:43:04 -0700

    recent 5 sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,17 +12,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar(43);
+		_putchar('+');
 		return (1);
 	}
-	else if (n < 0)
+	else if (n == 0)
 	{
-		putchar(45);
-		return (-1);
+		_putchar('0');
+		return (0);
 	}
 	else
 	{
-		putchar(48);
-		return (0);
+		_putchar('-');
+		return (-1);
 	}
 }
